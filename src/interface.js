7e3e2cbe9a86090fff5e06e14069965a1fa5697a
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-27 18:51:26 -0300

    c12- adding new modify

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,10 @@
 var PontoA = { x: 1, y: 0 };
+// practice more about this!!
 var felipe = {
     name: 'felipe',
-    idade: '20',
-    sexo: 'f'
+    idade: '20'
 };
+function ola(pessoa) {
+    console.log('ola', pessoa.name);
+}
+ola({ name: 'ola programmer', sexo: 'f' });
