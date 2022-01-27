c2958fca6d7a55085a7da64be764f52f2624e237
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-27 19:14:24 -0300

    c15- adding about interface to practice more interface and documentatio

--- src/interface.ts
+++ src/interface.ts
@@ -9,19 +9,29 @@ interface Point {
 }
 
 const PontoA : Point = {x:1, y:0}
-
 interface Pessoa {
     name: string,
     [key:string]: string | number ,
     sexo?: 'm' | 'f'
 }
+
 // practice more about this!!
+//explicito
 const felipe : Pessoa ={
     name: 'felipe',
     idade:'20'
 }
-
 function ola(pessoa: Pessoa){
     console.log('ola', pessoa.name)
 }
+//this called documentation
 ola({name:'ola programmer', sexo:'f'})
+
+const professor: Pessoa = {
+    name: 'Joaquim',
+    idade:'50',
+}
+function saudacao(pessoa: Pessoa){
+    console.log('hi teach', pessoa.name)
+}
+saudacao({name:'francisco', sexo: 'm'})
