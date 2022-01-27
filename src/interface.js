304e99abcdb07a348e043181f69da9abb09fa157
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-27 19:13:24 -0300

    c14- updating type about javascript

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,6 @@
 var PontoA = { x: 1, y: 0 };
 // practice more about this!!
+//explicito
 var felipe = {
     name: 'felipe',
     idade: '20'
@@ -7,4 +8,13 @@ var felipe = {
 function ola(pessoa) {
     console.log('ola', pessoa.name);
 }
+//this called documentation
 ola({ name: 'ola programmer', sexo: 'f' });
+var professor = {
+    name: 'Joaquim',
+    idade: '50'
+};
+function saudacao(pessoa) {
+    console.log('hi teach', pessoa.name);
+}
+saudacao({ name: 'francisco', sexo: 'm' });
