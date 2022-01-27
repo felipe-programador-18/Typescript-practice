b53f236143e52cea1b471f1048766891ec347467
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-27 16:43:54 -0300

    c1- started learning and practice more about typescript

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var a = 10;
+a = 'string';
+var city = 'porto alegre';
+city = 19;
+function Soma(E, F) {
+    return E + F;
+}
+console.log(Soma);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let a  = 10
+a = 'string'
+
+var city = 'porto alegre'
+city = 19
+
+
+function Soma(E:number,F: number){
+    return E+F
+}
+console.log(Soma)
