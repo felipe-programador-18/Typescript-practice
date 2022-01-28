9780568a736710876ca9445cffc862f8ff0982f6
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-28 09:48:48 -0300

    c24- change file little bit

--- dist/interface.js
+++ dist/interface.js
@@ -1,3 +1,4 @@
+// this mode explicito!!
 var PontoA = { x: 1, y: 0 };
 // practice more about this!!
 //explicito
@@ -26,3 +27,9 @@ kinda = 10;
 //i have show of one way better to built production
 var Other = 'Felipe';
 Other = 19;
+var empregado = {
+    adress: 'avenida circular',
+    city: 'porto alegre',
+    zipcode: 921,
+    complementer: 'apartamento'
+};
