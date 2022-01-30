57ae8c63a52b259193bd1eaf89642ad0c8bf7960
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-30 17:57:35 -0300

    c44- save documentation about folder

---
+++ src/inte.ts
@@ -0,0 +1,22 @@
+
+// need create interface to together this class
+
+interface Book {
+    nomebook: string;
+    ibm: number;
+    quantidade: number;
+    sumbook: number;
+    }
+    
+    interface SALE {
+      pesquisa: string;
+      update: string;
+    }
+    
+    interface Finnalysale {
+    search:string ;
+    print: string;
+    }
+    
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
