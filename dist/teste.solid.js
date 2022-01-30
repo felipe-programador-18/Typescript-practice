d8000a4b75ec3dcd9a2d73131a0d5e7a5e5641e8
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-30 16:28:10 -0300

    c38- try ir new interface to new class

---
+++ dist/teste.solid.js
@@ -0,0 +1,19 @@
+// need create interface to together this class
+var Libraries = /** @class */ (function () {
+    function Libraries() {
+        return this;
+    }
+    return Libraries;
+}());
+var Organizesale = /** @class */ (function () {
+    function Organizesale() {
+        return this;
+    }
+    return Organizesale;
+}());
+var Salefinal = /** @class */ (function () {
+    function Salefinal() {
+        return this;
+    }
+    return Salefinal;
+}());
