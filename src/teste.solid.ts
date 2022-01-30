203bbe45f47914652bc3a772f7817461d0887d16
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-30 16:28:33 -0300

    c39- together interface and class about typescript

---
+++ src/teste.solid.ts
@@ -0,0 +1,42 @@
+// need create interface to together this class
+
+interface Book {
+nomebook: string,
+ibm: number,
+quantidade: number,
+Somalivro: number
+}
+
+interface SALE {
+  pesquisa: string,
+  [key:string]: string | number,
+  update: string
+}
+
+interface Finnalysale {
+search:string ,
+print: string
+}
+
+
+
+
+
+
+class Libraries {
+    constructor(){
+     return this
+    }
+}
+
+class Organizesale {
+    constructor(){
+        return this
+    }
+}
+
+class Salefinal {
+    constructor(){
+        return this
+    }
+}
