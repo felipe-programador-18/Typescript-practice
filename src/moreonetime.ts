31b5c9433fa20543cf86770d651910f77f20334f
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 13:01:25 -0300

    c54- try it more one time classe and interface

---
+++ src/moreonetime.ts
@@ -0,0 +1,43 @@
+//Guidance about  OCP (-open-closed-Principle)
+interface Notafiscal{
+    valuebook:number;
+    namebook: string;
+}
+
+//Guidance about SRP (SINGLE RESPONSIBILITY PRINCIPLE)
+class Libraries {
+    constructor(nomebook:string){
+     this.nomebook = nomebook
+    }
+    nomebook: string;
+}
+
+class Pricebook implements Notafiscal {
+    valuebook:number;
+    namebook: string;
+    
+    constructor(pricebook:number){
+     this.pricebook = pricebook
+    }
+    pricebook: number;
+}
+
+class Finnaly implements Notafiscal {
+    valuebook:number;
+    namebook: string;
+    
+    constructor(bookone: number){
+        this.bookone = bookone 
+    }
+    bookone:number;
+}
+
+
+
+
+
+
+
+let fun1: Libraries = new Libraries('Learning Mysql')
+let prec: Pricebook = new Pricebook(99.90)
+let finasale: Finnaly = new Finnaly(99.90)
