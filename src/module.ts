4eb21f8105307714b07678b6153417be3ec03c62
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-02 19:41:28 -0300

    c61- did process about express

---
+++ src/module.ts
@@ -0,0 +1,14 @@
+//very interesting express it works inside typescript!!
+import * as Express from 'express'
+
+const app: Express.Application = Express()
+
+//always request first later response
+app.get('/', (req:Express.Request , res: Express.Response) =>{
+   res.send('hi there i am Felipe programmer18')
+})
+
+
+app.listen(8080, err => {
+    console.log('running')
+})
