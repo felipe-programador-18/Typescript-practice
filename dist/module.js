fa027b66e10378e8649994f7aac943587aba4806
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-06 14:34:37 -0300

    c87- adding new modigy

--- dist/module.js
+++ dist/module.js
@@ -1,31 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 //very interesting express it works inside typescript!!
-const Express = __importStar(require("express"));
-const app = Express();
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 //always request first later response
 app.get('/', (req, res) => {
     res.send('hi there i am Felipe programmer18');
 });
-app.listen(8080, err => {
-    console.log('running');
+app.listen(8080, () => {
+    console.log('running new versions');
 });
