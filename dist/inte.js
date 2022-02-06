14c5503f7ea12a53e216ae2905913402b90d3156
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-06 12:43:55 -0300

    c77- adding new code about javascript

--- dist/inte.js
+++ dist/inte.js
@@ -1,2 +1,9 @@
 var assey = 10;
 var Union = { x: 10, y: 30, z: 10 };
+var Structure = /** @class */ (function () {
+    function Structure(size) {
+        this.size = size;
+    }
+    return Structure;
+}());
+var Tooth = new Structure(1203);
