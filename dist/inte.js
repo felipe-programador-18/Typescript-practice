e6658b17343ae1a01e7c4c8bc616e384c37ca146
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-06 14:34:13 -0300

    c86- adding new insight about build

--- dist/inte.js
+++ dist/inte.js
@@ -1,19 +1,18 @@
+"use strict";
 var assey = 10;
-var Union = { x: 10, y: 30, z: 10 };
-var Structure = /** @class */ (function () {
-    function Structure(size) {
+const Union = { x: 10, y: 30, z: 10 };
+class Structure {
+    constructor(size) {
         this.size = size;
     }
-    return Structure;
-}());
-var Tooth = new Structure(1203);
-var data = { him: 'Felipe programmer', age: 40, gender: 'm', email: 'machadofelipe2016@outlook.com' };
-var Companies = /** @class */ (function () {
-    function Companies(him, age, gender) {
+}
+let Tooth = new Structure(1203);
+const data = { him: 'Felipe programmer', age: 40, gender: 'm', email: 'machadofelipe2016@outlook.com' };
+class Companies {
+    constructor(him, age, gender) {
         this.him = him;
         this.age = age;
         this.gender = gender;
     }
-    return Companies;
-}());
-var Togemore = new Companies('martins', 32, 'm');
+}
+let Togemore = new Companies('martins', 32, 'm');
