c70e376129643db92c5ce17c955555d806d06c7b
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-12 19:00:01 -0300

    c90- adding new code to practice more

--- dist/sale.js
+++ dist/sale.js
@@ -1,1 +1,6 @@
 "use strict";
+// remmerber about way inplicit
+var Bul = 'string';
+Bul = 12;
+let Fel = 12;
+Fel = 123;
---
+++ src/sale.ts
@@ -0,0 +1,8 @@
+
+// remmerber about way inplicit
+var Bul  = 'string'
+Bul = 12
+
+
+let Fel : number = 12
+Fel = 123
