c6ae2c51fda819703ffaee136f7b7d8cbbdd5ff8
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-12 19:04:24 -0300

    c91- create new interface to practice more my another language

--- dist/sale.js
+++ dist/sale.js
@@ -2,5 +2,7 @@
 // remmerber about way inplicit
 var Bul = 'string';
 Bul = 12;
+// this way explicit
 let Fel = 12;
 Fel = 123;
+const Captur = { cor: 'red', core: 12, coreone: 's' };
--- src/sale.ts
+++ src/sale.ts
@@ -3,6 +3,14 @@
 var Bul  = 'string'
 Bul = 12
 
-
+// this way explicit
 let Fel : number = 12
 Fel = 123
+
+interface Mine {
+    cor:string
+    core:number,
+    coreone?: 's' | 'n'
+}
+
+const Captur : Mine = { cor:'red', core:12, coreone:'s'}
